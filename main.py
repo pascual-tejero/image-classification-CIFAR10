ea55066b8938c7b4dcfdb190a073d505e06e2c1b
Pascual <116434029+pascutc98@users.noreply.github.com>
2023-10-30 13:52:06 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,43 +4,59 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.nn as nn
 
-
 import os
 
 from net import Net
 
+PATH = './cifar_net.pth' # Path to save the trained model
+
 def main():
+    # Set the environment variable to allow the use of the MKL library
     os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-
+    # Transform the data to tensors and normalize it    
     transform = transforms.Compose(
         [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    ) 
 
-    batch_size = 4
+    batch_size = 4 # Batch size
 
-    if os.path.exists("./data") == False:
-        os.mkdir("./data")
+    if os.path.exists("./data") == False: # Check if the data directory exists
+        os.mkdir("./data") # Create the data directory
 
+    # Download the CIFAR10 training dataset.
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
+                                        download=True, transform=transform)
+    
+    # There are 50,000 images in the training set. We will use 45,000 for training and 5,000 for validation
+    trainset, valset = torch.utils.data.random_split(trainset, [45000, 5000])
 
+    # Load the training and validation data into the memory
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2) 
+    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    
+    # Download the CIFAR10 dataset and load it into the memory
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=False, num_workers=2)
+                                            shuffle=False, num_workers=2) 
 
-    net = Net()
+    net = Net() # Create the network
     
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss() # Loss function
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) # Optimizer
     
+    print("---------------------------------------")
     print('Start training')
-    for epoch in range(5):  # loop over the dataset multiple times
+    for epoch in range(20):  # loop over the dataset multiple times
 
-        running_loss = 0.0
+        running_loss = 0.0 # Loss for each epoch
+        print("---------------------------------------")
+
+        # Train the network
         for i, data in enumerate(trainloader, 0):
                   
             # get the inputs; data is a list of [inputs, labels]
@@ -60,21 +76,59 @@ def main():
             if i % 2000 == 1999:    # print every 2000 mini-batches
                 print(f'Epoch: {epoch + 1}, Batch: {i + 1:5d} -> Loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
+        
+        # Validate the network
+        running_loss = 0.0
+        for i, data in enumerate(valloader, 0):
+            inputs, labels = data
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+        
+        print(f'Epoch: {epoch + 1}, Validation Loss: {running_loss / len(valloader):.3f}')
+
+        # Save the model with the lowest validation loss
+        if i == len(valloader) - 1:
+            if epoch == 0:
+                lowest_loss = running_loss # Set the lowest loss to the first loss
+            elif running_loss < lowest_loss:
+                lowest_loss = running_loss
+                torch.save(net.state_dict(), PATH) # Save the trained model
 
     print('Finished Training')
     
-    PATH = './cifar_net.pth'
-    torch.save(net.state_dict(), PATH)
-
     # Test the network on the test data
-    overall_loss = 0.0
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        loss = criterion(outputs, labels)
-        overall_loss += loss.item()
-    print(f'Overall loss test: {overall_loss / len(testloader):.3f}')
+    print("---------------------------------------")
+    print('Start testing')
 
+    # Load the best model
+    net.load_state_dict(torch.load(PATH))
+
+    tp = 0 # True Positive
+    tn = 0 # True Negative
+    fp = 0 # False Positive
+    fn = 0 # False Negative
+
+    for data in testloader:
+        images, labels = data # Get the images and labels from the test data
+        outputs = net(images) # Get the output from the network
+        _, predicted = torch.max(outputs.data, 1) # Get the class with the highest probability
+        tp += ((predicted == labels) & (predicted == 1)).sum().item() # True Positive
+        tn += ((predicted == labels) & (predicted == 0)).sum().item() # True Negative 
+        fp += ((predicted != labels) & (predicted == 1)).sum().item() # False Positive
+        fn += ((predicted != labels) & (predicted == 0)).sum().item() # False Negative
+
+    # Print the results    
+    print("---------------------------------------")
+    print(f'TP: {tp}, TN: {tn}, FP: {fp}, FN: {fn}') 
+    print(f'Accuracy: {(tp + tn) / (tp + tn + fp + fn):.3f}')
+    print(f'Precision: {tp / (tp + fp):.3f}')
+    print(f'Recall: {tp / (tp + fn):.3f}')
+    print(f'Specificity: {tn / (tn + fp):.3f}')
+    print(f'F1: {tp / (tp + (fp + fn) / 2):.3f}')
+
+    print('Finished Testing')
+    print("---------------------------------------")
 
 if __name__ == '__main__':
     main()
--- net.py
+++ net.py
@@ -1,27 +1,67 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class Net(nn.Module):
-    "Fully connected neural network for a input image: torch.Size([4, 3, 32, 32])"
+    """
+    The network architecture is based on the LeNet-5 architecture. 
+
+    The network consists of the following layers:
+    1. Convolutional layer (6 filters, 5x5 kernel, ReLU activation function)
+    2. Max pooling layer (2x2 kernel)
+    3. Convolutional layer (16 filters, 5x5 kernel, ReLU activation function)
+    4. Max pooling layer (2x2 kernel)
+    5. Fully connected layer (120 neurons, ReLU activation function)
+    6. Dropout layer (20% dropout probability)
+    7. Fully connected layer (84 neurons, ReLU activation function)
+    8. Dropout layer (20% dropout probability)
+    9. Fully connected layer (10 neurons, Softmax activation function)
+
+    The network uses the Cross Entropy Loss function and the SGD optimizer.
+
+    Output:
+        torch.Size([4, 10]) -> Probability of each class for each image in the batch (4 images, 10 classes)
+    """
 
     def __init__(self):
         super(Net, self).__init__()
         # 3 input image channel, 6 output channels, 5x5 square convolution kernel
         # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-        self.conv1 = nn.Conv2d(3, 6, 5) # torch.Size([4, 6, 28, 28])
-        self.pool = nn.MaxPool2d(2, 2) # torch.Size([4, 6, 14, 14])
-        self.conv2 = nn.Conv2d(6, 16, 5) # torch.Size([4, 16, 10, 10])
-        self.fc1 = nn.Linear(16 * 5 * 5, 120) # torch.Size([4, 120])
-        self.fc2 = nn.Linear(120, 84) # torch.Size([4, 84])
-        self.fc3 = nn.Linear(84, 10) # torch.Size([4, 10])
+
+        self.conv1 = nn.Conv2d(3, 6, 5) # 3 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(6, 16, 5) # 6 input image channel, 16 output channels, 5x5 square convolution kernel
+        self.max_pool = nn.MaxPool2d(2, 2) # Max pooling over a (2, 2) window
+
+        self.fc1 = nn.Linear(16 * 5 * 5, 120) # an affine operation: y = Wx + b
+        self.fc2 = nn.Linear(120 , 84) # an affine operation: y = Wx + b
+        self.fc3 = nn.Linear(84, 10) # an affine operation: y = Wx + b
+
+        self.dropout = nn.Dropout(0.2) # Dropout layer with 20% dropout probability
+        self.relu = nn.ReLU() # ReLU activation function
+        self.softmax = nn.Softmax(dim=1) # Softmax activation function
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        # Convolutional layers
+        x = self.conv1(x) # Input: torch.Size([4, 3, 32, 32]) -> Output: torch.Size([4, 6, 28, 28])
+        x = self.relu(x) # Input: torch.Size([4, 6, 28, 28]) -> Output: torch.Size([4, 6, 28, 28])
+        x = self.max_pool(x) # Input: torch.Size([4, 6, 28, 28]) -> Output: torch.Size([4, 6, 14, 14])
+
+        x = self.conv2(x) # Input: torch.Size([4, 6, 14, 14]) -> Output: torch.Size([4, 16, 10, 10])
+        x = self.relu(x) # Input: torch.Size([4, 16, 10, 10]) -> Output: torch.Size([4, 16, 10, 10])
+        x = self.max_pool(x) # Input: torch.Size([4, 16, 10, 10]) -> Output: torch.Size([4, 16, 5, 5])
+
+        # Flatten the image
+        x = x.view(-1, 16 * 5 * 5) # Input: torch.Size([4, 16, 5, 5]) -> Output: torch.Size([4, 400])
+
+        # Fully connected layers
+        x = self.fc1(x) # Input: torch.Size([4, 400]) -> Output: torch.Size([4, 120])
+        x = self.relu(x) # Input: torch.Size([4, 120]) -> Output: torch.Size([4, 120])
+        x = self.dropout(x) # Input: torch.Size([4, 120]) -> Output: torch.Size([4, 120])
+
+        x = self.fc2(x) # Input: torch.Size([4, 120]) -> Output: torch.Size([4, 84])
+        x = self.relu(x) # Input: torch.Size([4, 84]) -> Output: torch.Size([4, 84])
+        x = self.dropout(x) # Input: torch.Size([4, 84]) -> Output: torch.Size([4, 84])
+
+        x = self.fc3(x) # Input: torch.Size([4, 84]) -> Output: torch.Size([4, 10])
+        x = self.softmax(x) # Input: torch.Size([4, 10]) -> Output: torch.Size([4, 10])
+
         return x
     
